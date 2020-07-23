eb9509e98c5771717a3bb36e78aedd14ebba7026
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-07-23 08:59:14 -0400

    new

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace titan_of_industry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
